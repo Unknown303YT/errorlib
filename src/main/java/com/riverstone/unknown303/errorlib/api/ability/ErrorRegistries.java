057326f1cb0c624209aeed9731bdcf4d19e971a9
Unknown_303YT <unknown.303yt@gmail.com>
2025-04-17 10:38:42 +0930

    Delete ErrorLib main code from branch Abilities-Dev.
    Signed-off-by: Unknown_303YT <unknown.303yt@gmail.com>

--- src/main/java/com/riverstone/unknown303/errorlib/api/ability/ErrorRegistries.java
+++
@@ -1,9 +0,0 @@
-package com.riverstone.unknown303.errorlib.api.ability;
-
-import com.riverstone.unknown303.errorlib.ModHelpers;
-import net.minecraftforge.registries.IForgeRegistry;
-
-public class ErrorRegistries {
-    public static final IForgeRegistry<Ability> ABILITIES =
-            ModHelpers.REGISTRY_HELPER.createRegistry("ability", Ability::new);
-}
